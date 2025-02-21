2ffffee7f7463abc57accb1b44e8079be7c099d6
Mark Goadrich <goadrich@hendrix.edu>
2025-02-21 09:30:35 -0600

    Lab 3 updates, and other stuff

--- examples/pigDiceGame/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module pigDiceGame {
-    requires javafx.fxml;
-    requires javafx.controls;
-    exports pig.gui;
-    opens pig.gui;
-    exports pig.model;
-    opens pig.model;
-
-}
--- examples/virusSim/src/module-info.java
+++
@@ -1,6 +0,0 @@
-module virusSim {
-    requires javafx.fxml;
-    requires javafx.controls;
-    exports virus.gui;
-    opens virus.gui;
-}
